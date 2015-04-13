231f840ea82aaa799bcb88b8cc71d8727379151c
Callum Macrae <callum@macr.ae>
2015-04-13 12:06:06 +0100

    never rebuilding u again

--- src/js/load-time-graph.js
+++ src/js/load-time-graph.js
@@ -3,32 +3,34 @@
 var Chartist = require('chartist');
 var util = require('./util');
 
-var data = util.takeRight(require('../data.json').data, 30);
+util.getJson('src/data.json', function (res) {
+  var data = util.takeRight(res.data, 30);
 
-var options = {
-  low: 0,
-  axisY: {
-    labelInterpolationFnc: function (value) {
-      return value + 's';
-    }
-  },
-  lineSmooth: false
-};
+  var options = {
+    low: 0,
+    axisY: {
+      labelInterpolationFnc: function (value) {
+        return value + 's';
+      }
+    },
+    lineSmooth: false
+  };
 
-var loadData = {
-  labels: util.pluck(data, 'label'),
-  series: [
-    util.pluck(data, 'uncached'),
-    util.pluck(data, 'cached')
-  ]
-};
+  var loadData = {
+    labels: util.pluck(data, 'label'),
+    series: [
+      util.pluck(data, 'uncached'),
+      util.pluck(data, 'cached')
+    ]
+  };
 
-var displayData = {
-  labels: util.pluck(data, 'label'),
-  series: [
-    util.pluck(data, 'display')
-  ]
-};
+  var displayData = {
+    labels: util.pluck(data, 'label'),
+    series: [
+      util.pluck(data, 'display')
+    ]
+  };
 
-new Chartist.Line('.chart-load', loadData, options);
-new Chartist.Line('.chart-display', displayData, options);
+  new Chartist.Line('.chart-load', loadData, options);
+  new Chartist.Line('.chart-display', displayData, options);
+});
