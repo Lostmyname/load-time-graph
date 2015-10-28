8273b58ce75b7016383f5082459b507b805c3fa9
Callum Macrae <callum@macr.ae>
2015-10-28 11:28:12 +0000

    updated lots of stuff, including gulp -> 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,22 @@ var loadLmnTask = require('lmn-gulp-tasks');
 
 var buildPath = './demo/build/';
 
-gulp.task('auto-reload', loadLmnTask('auto-reload'));
-
-gulp.task('js', ['js-quality'], loadLmnTask('browserify', {
-  src: './src/js/load-time-graph.js',
-  dest: path.join(buildPath, 'js/bundle.js')
-}));
-
 gulp.task('js-quality', loadLmnTask('js-quality', {
   src: './src/js/**/*.js'
 }));
+
+var jsOpts = {
+  src: './src/js/load-time-graph.js',
+  dest: path.join(buildPath, 'js/bundle.js')
+};
+
+gulp.task('js', gulp.series('js-quality', loadLmnTask('browserify', jsOpts)));
+
+gulp.task('js-watch', gulp.series('js-quality', loadLmnTask('browserify', {
+  src: jsOpts.src,
+  dest: jsOpts.dest,
+  watch: true
+})));
 
 gulp.task('scss', loadLmnTask('scss', {
   src: './src/scss/*.{sass,scss}',
@@ -24,9 +30,9 @@ gulp.task('scss', loadLmnTask('scss', {
   imagePath: '../images'
 }));
 
-gulp.task('build', ['js', 'scss']);
+gulp.task('build', gulp.parallel('js', 'scss'));
 
-gulp.task('default', ['build'], function () {
+function watchers() {
   var config = {
     server: {
       baseDir: '.'
@@ -50,8 +56,12 @@ gulp.task('default', ['build'], function () {
     'test/**/*.js'
   ], config);
 
-  gulp.watch('./src/scss/**/*.{sass,scss}', ['scss']);
-  gulp.watch('./src/**/*.js{on,}', ['js']);
-  gulp.watch('./src/partials/partial.erb.html', ['html']);
-  gulp.watch('./demo/base.erb.html', ['html']);
-});
+  gulp.watch('./src/scss/**/*.{sass,scss}', gulp.series('scss'));
+}
+
+gulp.task(watchers);
+
+gulp.task('default', gulp.series(
+  'build',
+  gulp.parallel('js-watch', 'watchers')
+));
